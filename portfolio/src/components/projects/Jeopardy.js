6734f11252a512682e72f3a89a6fcf996aad1d0d
JarredStanford <stanj89@gmail.com>
2020-02-06 22:09:01 -0500

    updated with new projects.

--- portfolio/src/components/projects/Jeopardy.js
+++ portfolio/src/components/projects/Jeopardy.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import designComponents from '../../utils/designComponents'
 
 const Jeopardy = () => {
+
+    const { ButtonDiv, CustomText, MarginedButton } = designComponents()
+
     return (
-        <></>
+        <div style={{ margin: 'auto' }}>
+            <ButtonDiv>
+                <MarginedButton>Deployed Site</MarginedButton>
+                <MarginedButton>GitHub</MarginedButton>
+            </ButtonDiv>
+            <CustomText>
+                <p>Jeopardy aims to present an experience similar to actual Jeopardy by feeding random questions scraped from the j-archive.
+                </p>
+                <p>This project was built using React, semantic-ui and styled-components.</p>
+
+                <p>
+                    Feature Roadmap includes the ability to play full episodes, permanent score tracking, multiplayer and more.
+                </p>
+            </CustomText>
+        </div>
     )
 }
 
--- portfolio/src/components/projects/OffsiteSales.js
+++ portfolio/src/components/projects/OffsiteSales.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import designComponents from '../../utils/designComponents'
 
 const OffsiteSales = () => {
+
+    const { ButtonDiv, CustomText, MarginedButton } = designComponents()
+
     return (
-        <></>
+        <div style={{ margin: 'auto' }}>
+            <ButtonDiv>
+                <MarginedButton>Deployed Site</MarginedButton>
+                <MarginedButton>GitHub</MarginedButton>
+            </ButtonDiv>
+            <CustomText>
+                <p>Offsite Sales Tracker is an app that allows retail stores to track their sales that take place...well, offsite. This is currently being used in a commercial liquor store which sells items in the supermarket next door.</p>
+                <p>
+                    This project was built using React, semantic-ui, firebase, styled-components and momentJS.
+                </p>
+            </CustomText>
+        </div>
     )
 }
 
